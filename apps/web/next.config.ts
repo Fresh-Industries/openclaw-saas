c65bee13a229f58ca7459628d131fbfdb95cb59a
Nikolas Manuel <nikolas.manuel13@gmail.com>
2026-02-11 12:10:43 -0600

    Update to Next.js 16.1.6 (latest)

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": ["@svgr/webpack"],
-      },
-    },
-  },
   images: {
     remotePatterns: [
       {
